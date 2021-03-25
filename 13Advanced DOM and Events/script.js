02370ba90db92265d080b17b8fbc5040458ebe91
joker77z <50583262+joker77z@users.noreply.github.com>
2021-03-25 16:15:25 +0900

    183강 : 요소 선택, 생성 및 삭제
    prepand, append, before, after를 중점으로 알면 될듯 하다.

--- 13Advanced DOM and Events/script.js
+++ 13Advanced DOM and Events/script.js
@@ -28,3 +28,47 @@ document.addEventListener("keydown", function (e) {
     closeModal();
   }
 });
+
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+document.querySelector(".header");
+document.querySelectorAll(".section");
+console.log(allSections);
+
+document.queryElementById("section--1");
+const allButtons = document.getElementsByTagName("button");
+console.log(allButtons); // node형식과 또 다르게 보일 것이다.
+// 이것의 특징은 만약 웹 페이지 상에서 버튼요소를 delete로 삭제해버린 뒤
+// console창에 allButtons치면 요소 한개가 지워져 있을 것이다.
+// 이것과 반대로 노드는 웹 상에서 어떤 요소가 지워진다고 해서 변경되지 않는다.
+// 이것을 이용해서 나중에 getElemetsByTagName을 유용하게 사용할 수도 있다.
+
+console.log(document.getElemetsByClassName("btn"));
+// console창에 HTMLCollection 형식으로 모아지는데 유용하게 사용할 떄가 있다.
+// 그러나 보통 그냥 querySelector이 유용하다.
+
+// Cookie 물어보는 창 만들기
+const message = document.createElement("div");
+message.classList.add("cookie-message");
+message.innerHTML =
+  'We use cookied for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+//header.prepend(message);
+header.append(message);
+
+// 만약 message를 header안에서 첫번째 혹은 맨 마지막. 두 개 다 넣고 싶으면 복사를 해야 한다.
+//header.append(message.cloneNode(true));
+
+// header.before(mesasge) // 이전에 넣을 수도 있고 후에 넣을 수도 있다.
+// header.after(message);
+
+// Cookie 허락해서 닫기
+document
+  .querySelector(".btn--close-cookie")
+  .addEventListener("click", function () {
+    message.remove();
+    // 예전에는 이런방식으로 했다.
+    // mesasge.parentElement.removeChild(message);
+  });
