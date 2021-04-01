abf424447a1f8041ad046680df43dab5a6a3b4e0
joker77z <50583262+joker77z@users.noreply.github.com>
2021-04-01 16:22:46 +0900

    208. 내장 객체에 대한 프로토 타입 상속

--- 14OOP/script.js
+++ 14OOP/script.js
@@ -71,3 +71,69 @@ Person {firstName: "Jonas", birthYear: "1991"}
       constructor: ƒ (firstName, birthYear)
       __proto__: Object
 */
+
+// 208. 내장 객체에 대한 프로토 타입 상속 (207강 프로토 타입 체인 코드로.)---------------------------------------------------------
+console.log(jonas.__proto__); // Person의 prototype = Person.prototype
+console.log(jonas.__proto__.__proto__); // Object의 프로토 타입 = Object.prototype
+/*
+  {constructor: ƒ, __defineGetter__: ƒ, __defineSetter__: ƒ, hasOwnProperty: ƒ, __lookupGetter__: ƒ, …}
+    constructor: ƒ Object() // 여기서 보면 Object가 생성자인 것을 볼 수 있다.
+    hasOwnProperty: ƒ hasOwnProperty()
+    isPrototypeOf: ƒ isPrototypeOf()
+    propertyIsEnumerable: ƒ propertyIsEnumerable()
+    toLocaleString: ƒ toLocaleString()
+    toString: ƒ toString()
+    valueOf: ƒ valueOf()
+    __defineGetter__: ƒ __defineGetter__()
+    __defineSetter__: ƒ __defineSetter__()
+    __lookupGetter__: ƒ __lookupGetter__()
+    __lookupSetter__: ƒ __lookupSetter__()
+    get __proto__: ƒ __proto__()
+    set __proto__: ƒ __proto__()
+*/
+console.log(jonas.__proto__.__proto__.__proto__); // Object.prototype의 __proto__는 또 위로 올라가야하는데 없기 때문에 null이다.
+console.log(Person.prototype.constructor); // Person의 prototype으로 왔다가 다시 생성자인 Person으로 돌아간다.
+/*
+  ƒ (firstName, birthYear) {
+    // Instance Property
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+*/
+// 제대로 보기 위해서는 console.dir을 사용해야 한다.
+console.dir(Person.prototype.constructor);
+/*
+  ƒ Person(firstName, birthYear)
+    arguments: (...)
+    caller: (...)
+    length: 2
+    name: "Person"
+    prototype: {species: "Homo Sapiens", calcAge: ƒ, constructor: ƒ}
+    __proto__: ƒ ()
+    [[FunctionLocation]]: script.js:6
+    [[Scopes]]: Scopes[2]
+*/
+
+const arr = [3, 6, 4, 5, 6, 9, 3]; // new Array === []
+console.log(arr.__proto__); // constructor(생성자)가 Array다.
+console.log(arr.__proto__ === Array.prototype); // 배열을 만들면 Array 생성자에 의해 만들어지는 것을 알 수 있다.
+console.log(arr.__proto__.__proto__); // constructor(생성자)가 Object다.
+
+// 중복제거 기능.
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+// 이렇게 하면 이제 모든 배열이 해당 메소드를 상속받아서 호출할 수 있다.
+console.log(arr.unique());
+
+// 그런데 이건 안좋은 습관이다!
+// Array 자체 메소드로 unique가 다음 자바스크립트 버전에서 추가될 수 있다.
+
+const h1 = document.querySelector("h1");
+console.log(h1);
+console.dir(h1); // 맨 밑으로 내려보면 HTMLHeadingElement.
+// 맨 밑의 __proto__를 클릭해서 또 들어가면 HTMLElement
+// 또 다음은 Element 또 다음은 Node 또 다음은 EventTarget 또 다음은 Object.
+// 전 강의? 전전강의에 다이어그램 캡쳐해놓은 것을 확인해 볼 수 있다.
+
+console.dir((x) => x + 1); // __proto__로 들어가보면 apply, bind, 이런게 있음을 알 수 있다. 그리고 제일 밑 __proto__를 보면 Object라는 것을 알 수 있다. 즉, 함수 자체도 객체다.
