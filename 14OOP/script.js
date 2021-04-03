504ccbec997a6c257d71d66ea2117a5c98f6d1aa
joker77z <joker77z@naver.com>
2021-04-03 17:59:27 +0900

    212. 정적(static) 메서드
    어려운 부분이다.
    
    일단 기본 개념은 Array.from() 처럼 Array가 가지고 있는 고유 함수라는 것인데 [1, 2, 3].from()을 하면 오류가 나듯 상속이 되지 않는 다는 것이다.
    
    class 내부에서 정의할 때 static 함수명() 으로 정의를 하고 호출할때 클래스명.함수명() 으로 호출하면 된다.
    
    #1 처럼 일반 함수에도 적용이 가능한데 콘솔창에 jonas을 치면 hey에 대한 것은 나오지 않듯. 프로토 타입에 상속되지 않는다.
    
    이것을 사용해서 어디에 사용하는지는 아직 헤깔리는 부분이나. 이런 것이 있구나하고 이론만 알아두자.

--- 14OOP/script.js
+++ 14OOP/script.js
@@ -13,6 +13,7 @@ class PersonCl {
     this.birthYear = birthYear;
   }
 
+  // 인스턴스 메소드
   calcAge() {
     console.log(2021 - this.birthYear);
   }
@@ -25,6 +26,7 @@ class PersonCl {
     return 2021 - this.birthYear;
   }
 
+  // 유효성 검사 : 새로운 개체를 만들 때 유효성 검사를 할 때 유용하다. 그것이 세터가 하는 일.
   // this.fullName은 이미 존재하기 때문에 관습적으로 _fullName 이런식으로 한다. 자바스크립트 특별한 기능이 아닌 네이밍을 다르게 하는 것.
   // fullName 인자 값으로 들어온 값이 this.fullName으로 가고 set fullName을 통해서 받은 name값을 this._fullName에다가 name값을 받는다.
   set fullName(name) {
@@ -35,6 +37,12 @@ class PersonCl {
 
   get fullName() { // console창에서 이름.fullName 을 쳐도 나오게 혹은 이런 특성을 이용하게
     return this._fullName;
+  }
+
+  // 스태틱(정적)메소드
+  static hey() {
+    console.log('hey there');
+    console.log(this);
   }
 }
 
@@ -68,6 +76,7 @@ const account = {
     this.movements.push(mov);
   }
   
+  
 };
 // 게터 : 새로운 값으로 얻었다.
 console.log(account.latest); // 뒤에 괄호() 붙이는거 아니다.
@@ -75,5 +84,35 @@ console.log(account.latest); // 뒤에 괄호() 붙이는거 아니다.
 // account.latest(50);
 account.latest = 50;
 console.log(account.movements);
-
 // 위에 예제에 대입을 해보자.
+
+// 212. 정적 메소드
+/* #1
+  console창에 Array.from(document.querySelectorAll('h1'))을 했을 때 배열로 h1을 반환해서 [h1] 이 나왔다.
+  중요한 포인트는 Array 생성자를 사용해서 from 메소드를 사용 했다는 것이다.
+  만약 [1, 2, 3].from() 을 한다면 오류가 발생할 것이다.
+
+  어떠한 배열을 사용했더라도 __proto__ 즉, Array.prototype에서 이 from을 볼 수 없을 것이다. 이건 Array 프로토타입 속성이 아니다.
+  Array 생성자 그 자체에 연결되 있는 것이다.
+  그냥 단순한 하나의 함수다. Array의 단순한 함수.
+
+  다른 예로는 Number.parseFloat(12) // 12 처럼. 단순한 Number의 함수다. 123.parseFloat은 안되고 그냥 단순히 Number의 함수다.
+*/
+
+const Person = function(firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+}
+const jonas = new Person('Jonas', 1991);
+// #2
+Person.hey = function() {
+  console.log('Hey there');
+  console.log(this);
+}
+Person.hey();
+console.log(jonas); // Person.hey가 상속되지 않은 것을 볼 수 있다. 즉, person.hey는 그냥 person의 하나의 함수가 된 것.
+// 일반배열.from 을 쓸 수 없고 Array.from 이 사용가능 한 것과 같은 원리다.
+// jonas.hey() // 이것 역시 당연히 안된다. 프로토타입에 person.hey가 들어간 것이 아니라서 상속이 안된다.
+
+// #3 다른 예제에도 또 적용하는데 클래스 안에 쉽게 적용하는 법. static을 붙인다.
+PersonCl.hey();
