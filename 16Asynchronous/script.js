30e12bf330a79ff4010509d8011c2264e3340188
joker77z <50583262+joker77z@users.noreply.github.com>
2021-04-12 14:55:46 +0900

    244. 첫 번째 AJAX 호출 : XMLHttpRequest
    기본적인 ajax를 호출하는 방법에 대해 배웠다.
    const request = new XMLHttpRequest();
    
    request.open('GET', 'AJAX링크');
    request.send();
    request.addEventListener('load' function() {
      const [data] = JSON.parse(this.responseText);
    const html = `~~~~`;
    contriesContainer.insertAdjacentHTML('beforeend', html);
    }

--- 16Asynchronous/script.js
+++ 16Asynchronous/script.js
@@ -4,29 +4,50 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
-// 243. 비동기 자바스크립트
-/*
-    동기는 코드가 한 줄씩 실행된다.
-    동기코드의 문제점은 alert같은 알람창을 만났을 때 코드 실행이 멈춘다는 것이다.  
-*/
+// 244. 첫번째 ajax 호출 : xmlhttprequest
 
-// 하지만 비동기 코드는 아래와 같이 color가 먼저 변경되고 5초 뒤에 문구가 바뀌는 것이다.
-// 5초가 지나면 콜백함수가 실행된다.
-const p = document.querySelector('.p')
-setTimeout(function() {
-    p.textContent = 'my name is taejoon!' 
-}, 5000);
-p.style.color = "red";
-p.style.fontSize = "30px";
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  // 데이터 전달 유형 선택. 여기서는 데이터를 얻는 것이니 GET.
+  // 두번째 인자로 깃허브에 public api에서 사용할 주소를 넣는다.
+  // ctrl+ f : rest countries 검색
+  // auth는 추가로 인증이 필요한지.
+  // CORS는 Cross Origin Resource Sharing.
+  // 즉, CORS가 없으면 타사 API에 액세스 할 수 없다.
+  request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
+  request.send();
+  // console.log(request.responseText); // 아무 데이터도 나오지 않는다. 왜냐면 아직 완전히 load가 되지 않은 상태여서 그런듯 하다.
+  // 요청 보낸 것이 다 load됬을 때 콜백함수 실행.
+  request.addEventListener('load', function () {
+    //   console.log(this.responseText); // JSON이 나타난다. 객체 형태지만 제대로 정리가 되어있지 않은 형태로 나타난다.
+    //그래서 JSON.parse를 사용한다.
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
 
-// 하지만 모든 콜백함수가 비동기로 작동하는 것은 아니다.
-// 예를 들어서 map 방법을 들었을 때 비동기가 아니다.
-// 시간 제한 설정과 같은 기능을 가진 것들만 비동기가 가능하다.
-const img = document.querySelector('.dog');
-img.src = 'img/img-1.jpg';
-img.addEventListener('load', function() {
-    img.classList.add('fadeIn');
-})
-img.style.width = '300px';
-p.style.width = '300px';
-
+    const html = `
+    <article class="country">
+            <img class="country__img" src="${data.flag}" />
+            <div class="country__data">
+              <h3 class="country__name">${data.name}</h3>
+              <h4 class="country__region">${data.region}</h4>
+              <p class="country__row"><span>👫</span>${(
+                data.population / 1000000
+              ).toFixed(1)}</p>
+              <p class="country__row"><span>🗣️ </span>${
+                data.languages[0].name
+              }</p>
+              <p class="country__row"><span>💰</span>${
+                data.currencies[0].code
+              }</p>
+            </div>
+          </article>
+          `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+getCountryData('korea');
+getCountryData('usa');
+getCountryData('germany');
+// 이렇게 하면 가끔 순서가 바뀌어서 나온다.
+// 다음 강의에 콜백지옥을 보여주겠다.
