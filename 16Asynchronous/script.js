2ade12b5619305c7cac67c9e54cb23d9388e72e8
joker77z <50583262+joker77z@users.noreply.github.com>
2021-04-13 16:36:12 +0900

    251. 수동으로 리젝티드 된 것을 처리
    키 포인트
    - throw new Error
    - 리팩토링

--- 16Asynchronous/script.js
+++ 16Asynchronous/script.js
@@ -28,101 +28,91 @@ const renderCountry = function (data, className = '') {
   countriesContainer.insertAdjacentHTML('beforeend', html);
   // countriesContainer.style.opacity = 1;
 };
-// const getCountryAndNeightbour = function (country) {
-//   // AJAX call country1
-//   const request = new XMLHttpRequest();
-//   request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
-//   request.send();
-//   request.addEventListener('load', function () {
-//     const [data, data2] = JSON.parse(this.responseText);
-//     // console.log(data);
-//     console.log(data2);
-
-//     // Render Country 1
-//     renderCountry(data2);
-
-//     // Get neighbour country (2개)
-//     const [neighbour] = data2.borders;
-//     console.log([neighbour]);
-
-//     if (!neighbour) return; // 존재하지 않으면 return해서 오류발생안되고 진행되게.
-
-//     // AJAX call country2
-//     const request2 = new XMLHttpRequest();
-//     request2.open('GET', `https://restcountries.eu/rest/v2/alpha/${neighbour}`);
-//     request2.send();
-//     request2.addEventListener('load', function () {
-//       // console.log(this.responseText);
-//       const data2 = JSON.parse(this.responseText); //data2를 배열에 넣지 않아야 오류가 나오지 않는다. 이유는 코드 방식으로 불러온 api의 경우 배열의 형식이 아니다.
-//       console.log(data2);
-
-//       renderCountry(data2, 'neighbour');
-//     });
-//   });
-// };
-// getCountryAndNeightbour('kor');
-// ES6부터 이 콜백지옥을 해결하기 위한 방법이 있다. 다음강의에서.
-
-// 247. ES6에서 ajax. fetch활용
-// 불러오기 위한 다양한 옵션을 여기서 줄 수 있으나 단순하게 URL만 전달할거라 간단하다.
-// const request = fetch('https://restcountries.eu/rest/v2/name/korea');
-// console.log(request);
-
-// const getCountryData = function (country) {
-//   fetch(`https://restcountries.eu/rest/v2/name/${country}`) // 1. promise를 반환하는 함수사용
-//     .then(function (response) {
-//       // 2. then메소드로 promise를 처리.
-//       console.log(response); // Type이 Response로 나온다.
-//       return response.json(); // 3. 실제로 response객체로 나오는 것을 읽으려면 json메소드를 사용해야 한다. return을 사용하면서 데이터 그 자체로 나온다.
-//     })
-//     .then(function (data) {
-//       console.log(data);
-//       renderCountry(data[1]);
-//     });
-// };
-// getCountryData('korea');
 
 const renderError = function (msg) {
   countriesContainer.insertAdjacentText('beforeend', msg);
-  // countriesContainer.style.opacity = 1;
 };
+
+const getJSON = function (url, errorMsg = 'something went wrong') {
+  return fetch(url).then(response => {
+    if (!response.ok) {
+      throw new Error(`${errorMsg} (${response.status})`);
+    }
+    return response.json();
+  });
+};
+
+/* 혹시 모르니 백업 and 학습용 */
+// const getCountryData = function (country) {
+//   //country1
+//   fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+//     .then(response => {
+//       console.log(response); // ok: false, status : 404
+//       if (!response.ok) {
+//         throw new Error(`Country not found ${response.status}`);
+//         // throw가 들어가면 return 처럼 즉시 기능을 종료하고 반환한다.
+//         // 즉시 이 then 메소드는 리젝티드 프라미스가 될 것이다.
+//         // 그리고 마지막에 있는 catch로 바로 전파될 것이다.
+//       }
+//       return response.json();
+//     })
+//     .then(data => {
+//       renderCountry(data[1]);
+//       // const neighbour = data[1].borders[0];
+//       const neighbour = 'dfwfewf';
+
+//       if (!neighbour) return;
+//       // country2
+//       return fetch(`https://restcountries.eu/rest/v2/alpha/${neighbour}`);
+//     })
+//     .then(response => {
+//       if (!response.ok) {
+//         throw new Error(`neighbour not found ${response.status}`);
+//       }
+//       return response.json();
+//     })
+//     .then(data => renderCountry(data, 'neighbour'))
+//     .catch(err => {
+//       console.error(`${err}!`);
+//       renderError(`something wrong : ${err.message}`); // err에 message만.
+//     })
+//     .finally(() => {
+//       countriesContainer.style.opacity = 1;
+//     });
+// };
 
 const getCountryData = function (country) {
   //country1
-  fetch(`https://restcountries.eu/rest/v2/name/${country}`)
-    .then(
-      response => response.json()
-      // err => alert(err) // 첫번째 방법. 여기서 오류를 캐치해서 console에 uncaught오류 발생이 x
-    )
+  getJSON(
+    `https://restcountries.eu/rest/v2/name/${country}`,
+    'Country not found'
+  )
     .then(data => {
-      renderCountry(data[1]);
-      const neighbour = data[1].borders[0];
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+      // console.log(neighbour); // 호주 이웃없다. undefined. catch핸들러에 걸린다.
+      // const neighbour = 'dfwfewf';
 
-      if (!neighbour) return;
+      if (!neighbour) throw new Error('No neighbour found!');
       // country2
-      return fetch(`https://restcountries.eu/rest/v2/alpha/${neighbour}`);
+      return getJSON(`https://restcountries.eu/rest/v2/alpha/${neighbour}`);
     })
-    .then(
-      response => response.json()
-      // arr => alert(arr) // 이 fetch를 가져올 때에 대한 arr 대응도 해준다.
-    )
     .then(data => renderCountry(data, 'neighbour'))
     .catch(err => {
-      console.error(`${err}!`); // 두번째. 더 좋은 방법. 오류는 기본적으로 잡힐 때 까지 쭉 밑으로 내려간다.
-      // 그리고 어디에도 잡히지 않을 경우 Uncaught 오류가 발생한다.
+      console.error(`${err}!`);
       renderError(`something wrong : ${err.message}`); // err에 message만.
     })
     .finally(() => {
       countriesContainer.style.opacity = 1;
-    }); // 풀필드나 리젝트 되더라도 항상 실행된다.
-  // 정리하면 풀필드일때는 then이 실행을.
-  // 리젝트 될 때는 catch 실행을.
-  // finally 는 항상. 매번 쓰이지는 않는다. 데이터를 로드할 때 작업이 완료되기 전에 보여주는 돌아가는 원 만들때 쓴다.
-  // opacity 1로 만드는 것을 여기다가 쓰자.
+    });
 };
-// 250. 거부 된 약속 처리
 btn.addEventListener('click', function () {
   getCountryData('korea');
 });
 
-getCountryData('koreaa'); // 이것을 입력했을 때 이 이름을 가진 국가를 찾을 수 없습니다. 라는 문구가 나오도록 다음강의에서 연습.
+// getCountryData('koreaaaaa'); // 오류를 일부러 발생시켜서 오류 페이지를 예방하는 방법 좋다.
+// 기억하자. 오류를 대비하기 위해 catch를 쓰고 필요 시 finally를 사용하는 것.
+// 이번엔 두번째 then에서 이웃 관련 오류를 만들어본다.
+// getJSON을 만들어서 리펙토링 한다.
+// 이웃나라가 없는 오스트렐리아의 경우를 만든다.
+getCountryData('australia');
