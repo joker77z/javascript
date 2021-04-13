9605b4a5f408a80df15095f9d09b28ac5729a4ca
joker77z <joker77z@naver.com>
2021-04-14 00:09:14 +0900

    252. 코딩챌린지 #1

--- 16Asynchronous/script.js
+++ 16Asynchronous/script.js
@@ -1,118 +1,74 @@
 'use strict';
 
-const btn = document.querySelector('.btn-country');
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+In this challenge you will build a function 'whereAmI' which renders a country ONLY based on GPS coordinates. For that, you will use a second API to geocode coordinates.
+
+Here are your tasks:
+
+PART 1
+1. Create a function 'whereAmI' which takes as inputs a latitude value (lat) and a longitude value (lng) (these are GPS coordinates, examples are below).
+2. Do 'reverse geocoding' of the provided coordinates. Reverse geocoding means to convert coordinates to a meaningful location, like a city and country name. Use this API to do reverse geocoding: https://geocode.xyz/api.
+The AJAX call will be done to a URL with this format: https://geocode.xyz/52.508,13.381?geoit=json. Use the fetch API and promises to get the data. Do NOT use the getJSON function we created, that is cheating 😉
+3. Once you have the data, take a look at it in the console to see all the attributes that you recieved about the provided location. Then, using this data, log a messsage like this to the console: 'You are in Berlin, Germany'
+4. Chain a .catch method to the end of the promise chain and log errors to the console
+5. This API allows you to make only 3 requests per second. If you reload fast, you will get this error with code 403. This is an error with the request. Remember, fetch() does NOT reject the promise in this case. So create an error to reject the promise yourself, with a meaningful error message.
+
+PART 2
+6. Now it's time to use the received data to render a country. So take the relevant attribute from the geocoding API result, and plug it into the countries API that we have been using.
+7. Render the country and catch any errors, just like we have done in the last lecture (you can even copy this code, no need to type the same code)
+
+TEST COORDINATES 1: 52.508, 13.381 (Latitude, Longitude)
+TEST COORDINATES 2: 19.037, 72.873
+TEST COORDINATES 2: -33.933, 18.474
+
+GOOD LUCK 😀
+*/
 const countriesContainer = document.querySelector('.countries');
 
-///////////////////////////////////////
-// 244. 첫번째 ajax 호출 : xmlhttprequest
-
 const renderCountry = function (data, className = '') {
-  const html = `
-    <article class="country ${className}">
-            <img class="country__img" src="${data.flag}" />
-            <div class="country__data">
-              <h3 class="country__name">${data.name}</h3>
-              <h4 class="country__region">${data.region}</h4>
-              <p class="country__row"><span>👫</span>${(
-                data.population / 1000000
-              ).toFixed(1)}</p>
-              <p class="country__row"><span>🗣️ </span>${
-                data.languages[0].name
-              }</p>
-              <p class="country__row"><span>💰</span>${
-                data.currencies[0].code
-              }</p>
+    const html = `
+      <article class="country ${className}">
+              <img class="country__img" src="${data.flag}" />
+              <div class="country__data">
+                <h3 class="country__name">${data.name}</h3>
+                <h4 class="country__region">${data.region}</h4>
+                <p class="country__row"><span>👫</span>${(
+                  data.population / 1000000
+                ).toFixed(1)}</p>
+                <p class="country__row"><span>🗣️ </span>${
+                  data.languages[0].name
+                }</p>
+                <p class="country__row"><span>💰</span>${
+                  data.currencies[0].code
+                }</p>
             </div>
-          </article>
-          `;
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  // countriesContainer.style.opacity = 1;
-};
+        </article>
+        `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  };
 
-const renderError = function (msg) {
-  countriesContainer.insertAdjacentText('beforeend', msg);
-};
-
-const getJSON = function (url, errorMsg = 'something went wrong') {
-  return fetch(url).then(response => {
-    if (!response.ok) {
-      throw new Error(`${errorMsg} (${response.status})`);
-    }
-    return response.json();
-  });
-};
-
-/* 혹시 모르니 백업 and 학습용 */
-// const getCountryData = function (country) {
-//   //country1
-//   fetch(`https://restcountries.eu/rest/v2/name/${country}`)
-//     .then(response => {
-//       console.log(response); // ok: false, status : 404
-//       if (!response.ok) {
-//         throw new Error(`Country not found ${response.status}`);
-//         // throw가 들어가면 return 처럼 즉시 기능을 종료하고 반환한다.
-//         // 즉시 이 then 메소드는 리젝티드 프라미스가 될 것이다.
-//         // 그리고 마지막에 있는 catch로 바로 전파될 것이다.
-//       }
-//       return response.json();
-//     })
-//     .then(data => {
-//       renderCountry(data[1]);
-//       // const neighbour = data[1].borders[0];
-//       const neighbour = 'dfwfewf';
-
-//       if (!neighbour) return;
-//       // country2
-//       return fetch(`https://restcountries.eu/rest/v2/alpha/${neighbour}`);
-//     })
-//     .then(response => {
-//       if (!response.ok) {
-//         throw new Error(`neighbour not found ${response.status}`);
-//       }
-//       return response.json();
-//     })
-//     .then(data => renderCountry(data, 'neighbour'))
-//     .catch(err => {
-//       console.error(`${err}!`);
-//       renderError(`something wrong : ${err.message}`); // err에 message만.
-//     })
-//     .finally(() => {
-//       countriesContainer.style.opacity = 1;
-//     });
-// };
-
-const getCountryData = function (country) {
-  //country1
-  getJSON(
-    `https://restcountries.eu/rest/v2/name/${country}`,
-    'Country not found'
-  )
+const whereAmI = function (latitude, longitude) {
+//   fetch('https://geocode.xyz/51.50354,-0.12768?geoit=json')
+    fetch(`https://geocode.xyz/${latitude},${longitude}?geoit=json`)
+    .then(response => {
+      if(!response.ok) throw new Error(`please stop request ${response.status}`);
+      return response.json();
+    })
     .then(data => {
-      renderCountry(data[0]);
-      const neighbour = data[0].borders[0];
-      // console.log(neighbour); // 호주 이웃없다. undefined. catch핸들러에 걸린다.
-      // const neighbour = 'dfwfewf';
+      console.log(data);
+      console.log(`You are in ${data.country}`);
 
-      if (!neighbour) throw new Error('No neighbour found!');
-      // country2
-      return getJSON(`https://restcountries.eu/rest/v2/alpha/${neighbour}`);
-    })
-    .then(data => renderCountry(data, 'neighbour'))
-    .catch(err => {
-      console.error(`${err}!`);
-      renderError(`something wrong : ${err.message}`); // err에 message만.
-    })
-    .finally(() => {
-      countriesContainer.style.opacity = 1;
-    });
+      return fetch(`https://restcountries.eu/rest/v2/name/${data.country}`)
+    }).then(response => {
+        if(!response.ok) throw new Error(`country not found ${response.status}`);
+        return response.json();
+    }).then(data => renderCountry(data[0]))
+    .catch(err => console.error(`some error happen : ${err.message}`));
 };
-btn.addEventListener('click', function () {
-  getCountryData('korea');
-});
-
-// getCountryData('koreaaaaa'); // 오류를 일부러 발생시켜서 오류 페이지를 예방하는 방법 좋다.
-// 기억하자. 오류를 대비하기 위해 catch를 쓰고 필요 시 finally를 사용하는 것.
-// 이번엔 두번째 then에서 이웃 관련 오류를 만들어본다.
-// getJSON을 만들어서 리펙토링 한다.
-// 이웃나라가 없는 오스트렐리아의 경우를 만든다.
-getCountryData('australia');
+whereAmI(52.508, 13.381);
+whereAmI(19.037, 72.873);
+whereAmI(-33.933, 18.4742211);
