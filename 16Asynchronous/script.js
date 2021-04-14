6bb0bfc12ff5b280b654bab0e82b77a6f17c5d73
joker77z <joker77z@naver.com>
2021-04-14 23:53:25 +0900

    257. 코딩 과제 # 2
    정확하게 promise에 대해서 완전 이해는 못했다. 유튜브로 추가적인 정보들을 얻어야 겠다.

--- 16Asynchronous/script.js
+++ 16Asynchronous/script.js
@@ -1,67 +1,77 @@
 'use strict';
 
-const btn = document.querySelector('.btn-country');
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+Build the image loading functionality that I just showed you on the screen.
+
+Tasks are not super-descriptive this time, so that you can figure out some stuff on your own. Pretend you're working on your own 😉
+
+PART 1
+1. Create a function 'createImage' which receives imgPath as an input. This function returns a promise which creates a new image (use document.createElement('img')) and sets the .src attribute to the provided image path. When the image is done loading, append it to the DOM element with the 'images' class, and resolve the promise. The fulfilled value should be the image element itself. In case there is an error loading the image ('error' event), reject the promise.
+
+If this part is too tricky for you, just watch the first part of the solution.
+
+PART 2
+2. Comsume the promise using .then and also add an error handler;
+3. After the image has loaded, pause execution for 2 seconds using the wait function we created earlier;
+4. After the 2 seconds have passed, hide the current image (set display to 'none'), and load a second image (HINT: Use the image element returned by the createImage promise to hide the current image. You will need a global variable for that 😉);
+5. After the second image has loaded, pause execution for 2 seconds again;
+6. After the 2 seconds have passed, hide the current image.
+
+TEST DATA: Images in the img folder. Test the error handler by passing a wrong image path. Set the network speed to 'Fast 3G' in the dev tools Network tab, otherwise images load too fast.
+
+GOOD LUCK 😀
+*/
+
+const imgDiv = document.querySelector('.images');
 const countriesContainer = document.querySelector('.countries');
 
-const renderCountry = function (data, className = '') {
-  const html = `
-    <article class="country ${className}">
-            <img class="country__img" src="${data.flag}" />
-            <div class="country__data">
-              <h3 class="country__name">${data.name}</h3>
-              <h4 class="country__region">${data.region}</h4>
-              <p class="country__row"><span>👫</span>${(
-                data.population / 1000000
-              ).toFixed(1)}</p>
-              <p class="country__row"><span>🗣️ </span>${
-                data.languages[0].name
-              }</p>
-              <p class="country__row"><span>💰</span>${
-                data.currencies[0].code
-              }</p>
-            </div>
-          </article>
-          `;
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
-};
+const createImage = function(imgPath) {
+  return new Promise(function(resolve, reject) {
+      const img = document.createElement('img');
+      img.src = imgPath;
+      img.addEventListener('load', function() {
+        imgDiv.appendChild(img);
+        resolve(img); // 얘가 없으면 then이 되지 않는다.
+      })
 
-const renderError = function (msg) {
-  countriesContainer.insertAdjacentText('beforeend', msg);
-};
-
-const getPosition = function () {
-  return new Promise(function (resolve, reject) {
-    navigator.geolocation.getCurrentPosition(resolve, reject);
-    // position => console.log(position),
-    // err => console.error(err)
+      img.addEventListener('error', function() {
+        reject(new Error('image not found'))
+      })
   });
-};
-getPosition().then(pos => console.log(pos.coords));
+}
 
-const whereAmI = function () {
-  getPosition()
-    .then(pos => {
-      const { latitude: lat, longitude: lng } = pos.coords;
-      return fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
-    })
-    .then(response => {
-      console.log(response);
-      if (!response.ok)
-        throw new Error(`please stop request ${response.status}`);
-      return response.json();
-    })
-    .then(data => {
-      console.log(data);
-      console.log(`You are in ${data.country.replace(/ /g, '')}`);
+const wait = function(seconds) {
+  return new Promise(function(resolve) {
+    // setTimeout('test', 2000 * seconds);
+    setTimeout(() => {
+      'test'
+    }, 2000 * seconds);
+  })
+}
 
-      return fetch(`https://restcountries.eu/rest/v2/name/${data.country}`);
-    })
-    .then(response => {
-      if (!response.ok) throw new Error(`country not found ${response.status}`);
-      return response.json();
-    })
-    .then(data => renderCountry(data[0]))
-    .catch(err => console.error(`some error happen : ${err.message}`));
-};
-btn.addEventListener('click', whereAmI);
+let currentImg;
+createImage('./img/img-1.jpg')
+.then(img => {
+  currentImg = img;
+  console.log('image 1 loaded');
+  return wait(2);
+})
+.then(() => {
+  currentImg.style.display = 'none';
+  return createImage('./img/img-2.jpg')
+})
+.then(img => {
+  currentImg = img;
+  console.log('image 2 loaded');
+  return wait(2);
+})
+.then(() => {
+  currentImg.style.display = 'none';
+})
+.catch(err => {
+  countriesContainer.insertAdjacentText('beforeend', err);
+  countriesContainer.style.opacity = 1;
+})
